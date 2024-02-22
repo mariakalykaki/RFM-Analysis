f881582b3ffb1555d5ee62f957b3e94a5914f611
Maria Kalikaki <122359543+mariakalykaki@users.noreply.github.com>
2024-02-22 16:49:31 +0200

    Update SQL code.sql

--- SQL code.sql
+++ SQL code.sql
@@ -79,7 +79,7 @@ from rfm_calc c
 select CUSTOMERNAME , rfm_recency, rfm_frequency, rfm_monetary,
 	case 
 		when rfm_cell_string in (111, 112 , 121, 122, 123, 132, 211, 212, 114, 141) then 'lost_customers'  --lost customers
-		when rfm_cell_string in (133, 134, 143, 244, 334, 343, 344, 144) then 'slipping away, cannot lose' -- (Big spenders who haven’t purchased lately) slipping away
+		when rfm_cell_string in (133, 134, 143, 244, 334, 343, 344, 144) then 'slipping away, cannot lose' -- (Big spenders who havenâ€™t purchased lately) slipping away
 		when rfm_cell_string in (311, 411, 331) then 'new customers'
 		when rfm_cell_string in (222, 223, 233, 322) then 'potential churners'
 		when rfm_cell_string in (323, 333,321, 422, 332, 432) then 'active' --(Customers who buy often & recently, but at low price points)
@@ -90,34 +90,7 @@ from #rfm
 
 
 
---What products are often sold together?
 
-
-
-
-select distinct OrderNumber, stuff(
-
-	(select ',' + PRODUCTCODE
-	from [dbo].[sales_data] p
-	where ORDERNUMBER in 
-		(
-
-			select ORDERNUMBER
-			from (
-				select ORDERNUMBER, count(*) rn
-				FROM [PortfolioDB].[dbo].[sales_data]
-				where STATUS = 'Shipped'
-				group by ORDERNUMBER
-			)m
-			where rn = 3
-		)
-		and p.ORDERNUMBER = s.ORDERNUMBER
-		for xml path (''))
-
-		, 1, 1, '') ProductCodes
-
-from [dbo].[sales_data] s
-order by 2 desc
 
 
 ---EXTRAs----
